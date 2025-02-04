f703cb07f80768e1cd40784c6f4a8195062c4119
Renan Ladislau <renan.lsousa@telefonica.com>
2025-02-04 14:47:02 -0300

    update: Adicionando novos estilos e componentes

--- client/src/components/Beneficios/Beneficios.jsx
+++ client/src/components/Beneficios/Beneficios.jsx
@@ -6,13 +6,22 @@ function Beneficios() {
     <div className='benefitsContainer'>
         <div>
         <h2>Beneficios</h2>
-        <p>Alguns Beneficios de utilizar Listas de Tarefas:</p>
+        <p id='subText_benefits'>Alguns Beneficios de utilizar Listas de Tarefas:</p>
         </div>
 
         <div className='itensContainer'>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+                <h3>Melhora da organização:</h3>
+                <p>Manter todas as suas tarefas em um só lugar ajuda a visualizar e priorizar suas atividades, evitando esquecimentos e sobrecargas</p>
+            </div>
+            <div>
+                <h3>Aumento da produtividade:</h3>
+                <p>Com um planejamento claro, você pode focar nas tarefas mais importantes e urgentes, otimizando seu tempo e esforço</p>
+            </div>
+            <div>
+                <h3>Redução do estresse:</h3>
+                <p></p>
+            </div>
         </div>
     </div>
   )
